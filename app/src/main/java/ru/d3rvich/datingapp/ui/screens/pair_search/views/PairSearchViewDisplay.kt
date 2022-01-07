617c14e42df6c2f1fad20a1f0f26e0c33810e8d7
D3rvich <ilya24.1999@gmail.com>
2022-01-07 21:38:38 +0500

    Update pair search screen (#27)

--- app/src/main/java/ru/d3rvich/datingapp/ui/screens/pair_search/views/PairSearchViewDisplay.kt
+++ app/src/main/java/ru/d3rvich/datingapp/ui/screens/pair_search/views/PairSearchViewDisplay.kt
@@ -3,6 +3,7 @@ package ru.d3rvich.datingapp.ui.screens.pair_search.views
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -10,15 +11,19 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import ru.d3rvich.datingapp.R
 import ru.d3rvich.datingapp.domain.entity.DateEntity
 import ru.d3rvich.datingapp.domain.entity.ProfileEntity
+import ru.d3rvich.datingapp.ui.constants.Personalities
+import ru.d3rvich.datingapp.ui.constants.Zodiac
 
 @Composable
 fun PairSearchViewDisplay(
@@ -71,25 +76,36 @@ fun PairSearchViewDisplay(
                     modifier = Modifier
                         .weight(1f)
                         .fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
                 ) {
+                    val backgroundModifier = Modifier
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color(red = 0f, green = 0.5f, blue = 0f))
+                        .padding(horizontal = 8.dp)
                     Row(
                         modifier = Modifier.fillMaxWidth(),
                         horizontalArrangement = Arrangement.SpaceEvenly
                     ) {
                         Text(
-                            text = currentCandidate.socionicTypeNumber.toString(),
-                            modifier = Modifier.padding(end = 4.dp)
+                            modifier = backgroundModifier,
+                            text = currentCandidate.fateNumber.toString()
                         )
+                        val zodiac = Zodiac.values()[currentCandidate.zodiacId]
                         Text(
-                            text = currentCandidate.fateNumber.toString(),
-                            modifier = Modifier.padding(start = 4.dp)
+                            modifier = backgroundModifier,
+                            text = stringResource(id = zodiac.stringRes)
                         )
                     }
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = currentCandidate.zodiacId.toString())
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(text = currentCandidate.personalitiesNumber.toString())
+                    Text(
+                        modifier = backgroundModifier,
+                        text = currentCandidate.socionicTypeNumber.toString()
+                    )
+                    val personality = Personalities.values()[currentCandidate.personalitiesNumber]
+                    Text(
+                        modifier = backgroundModifier,
+                        text = stringResource(id = personality.stringRes)
+                    )
                 }
                 IconButton(onClick = onLike) {
                     Icon(
